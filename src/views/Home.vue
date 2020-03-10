eef5894f2c8a06f63387e5f37513bc94d2a0f2c5
Andr3wRiv3rs <andrewarivers@gmail.com>
2020-03-10 15:04:18 -0700

    vue-todo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="(item, index) in list" :key="index">
+      <input v-model="list[index]" @keyup="save"/>
+      <button @click="removeInput(index)">Delete</button>
+    </div>
+
+    <br />
+
+    <button @click="logList">Log list</button>
+    <button @click="appendInput">Add todo</button>
+    <!-- <button @click="removeInput">Remove todo</button> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      list: ['']
+    }
+  },
+  
+  methods: {
+    logList () {
+      console.log(this.list)
+    },
+    
+    appendInput () {
+      this.list.push("")
+    },
+
+    removeInput (index) {
+      this.list.splice(index, 1)
+    },
+
+    save () {
+      localStorage.list = JSON.stringify(this.list)
+    },
+
+    load () {
+      if (localStorage.list) this.list = JSON.parse(localStorage.list)
+    },
+  },
+
+  mounted () {
+    this.load()
+  },
 }
 </script>
+
+<style scoped>
+/* .home > div::before {
+  content: '';
+  width: 10px;
+  height: 10px;
+  display: inline-block;
+  background-color: blue;
+} */
+
+.home > div > button {
+  opacity: 0;
+}
+
+.home > div:hover > button {
+  opacity: 1;
+}
+</style>
